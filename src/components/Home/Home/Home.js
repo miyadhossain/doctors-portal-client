de72086fb057644b985caaa3d64fed34a15b9fee
miyadhossain <miyad.mh@gmail.com>
2021-04-10 20:58:50 +0600

    Home page design done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Blogs from "../Blogs/Blogs";
+import Contact from "../Contact/Contact";
+import Doctors from "../Doctors/Doctors";
+import FeaturedService from "../FeaturedService/FeaturedService";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import MakeAppoientment from "../MakeAppoientment/MakeAppoientment";
 import Services from "../Services/Services";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <Services></Services>
+      <FeaturedService></FeaturedService>
+      <MakeAppoientment></MakeAppoientment>
+      <Testimonials></Testimonials>
+      <Blogs></Blogs>
+      <Doctors></Doctors>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 };
